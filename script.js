2866cf9b085b814848d967c4b76f4a20bd402711
Isla Nur <61099627+islanur@users.noreply.github.com>
2022-08-25 23:37:20 +0800

    Membuat program penjualan barang

---
+++ script.js
@@ -0,0 +1,39 @@
+// Program pembelian alat komputer
+let namaBarang, hargaSatuan, potongan, total;
+let kodeBarang = prompt('Masukkan kode barang :');
+let jumlahBeli = prompt('Masukkan jumlah pembelian : ');
+if (kodeBarang == 1) {
+	namaBarang = 'Mouse';
+	hargaSatuan = 75000;
+} else if (kodeBarang == 2) {
+	namaBarang = 'Keyboard';
+	hargaSatuan = 150000;
+} else {
+	namaBarang = 'Monitor';
+	hargaSatuan = 500000;
+}
+
+total = jumlahBeli * hargaSatuan;
+
+if (total > 150000 && namaBarang == 'Keyboard') {
+	potongan = 25000;
+} else {
+	potongan = 0;
+}
+
+total -= potongan;
+
+alert(
+	'Data Pembelian' +
+		'\n===========================' +
+		'\nNama Barang : ' +
+		namaBarang +
+		'\nHarga Satuan : ' +
+		hargaSatuan +
+		'\nJumlah Pembelian : ' +
+		jumlahBeli +
+		'\nPotongan : ' +
+		potongan +
+		'\nTotal Pembayaran : ' +
+		total
+);
